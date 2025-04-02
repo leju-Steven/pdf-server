6db556602b4b218a9d1bacae12912e681a3143c4
leju-Steven <steven@leju.com.tw>
2025-04-03 01:18:08 +0800

    setViewport

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.get("/pdf_report", async (req, res) => {
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
   );
+  await page.setViewport({ width: 1280, height: 800 });
   await page.setExtraHTTPHeaders({
     "Accept-Language": "zh-TW,zh;q=0.9",
   });
