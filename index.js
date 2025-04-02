3313353c076b46dad3b176973414f82e04cfdc87
leju-Steven <steven@leju.com.tw>
2025-04-03 00:13:37 +0800

    executablePath

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/pdf_report", async (req, res) => {
 
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: '/bin/chromium',
     defaultViewport: null, // 使用原生 viewport size
     args: [
       "--disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure",
